ef9dcce1c2ccc8184b673f742b187b48ae5b9374
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-04-30 19:11:13 +0200

    Added pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv().config(path.join("/etc", "hgwarmap.env"))
+
+module.exports = {
+    apps: [{
+        name: "hgwarmap",
+        script: "./dist/index.js",
+        env: {
+            NODE_ENV: "production",
+            HAG_USERNAME: String(process.env.HAG_USERNAME),
+            HAG_USERAGENT: String(process.env.HAG_USERAGENT),
+            HAG_PASSWORD: String(process.env.HAG_PASSWORD),
+        }
+    }],
+    deploy: {
+        production: {
+            "user": "root",
+            "host": [
+                "192.168.3.104"
+            ],
+            "ref": "origin/main",
+            "repo": "git@github.com:raouldeheer/hgwarmap.git",
+            "path": "/mnt/apps/hgwarmap",
+            "post-deploy": "npm run post-deploy && pm2 startOrRestart ecosystem.config.js --env production"
+        }
+    }
+};
