4d12281f86a97a2723530b80cb5b10dcd774871a
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-05-03 19:52:15 +0200

    Added more chunks

--- client/src/map/warmap.tsx
+++ client/src/map/warmap.tsx
@@ -6,10 +6,15 @@ import supplyline from "hagcp-assets/json/supplyline.json";
 import { WarmapEventHandler } from "../warmapEventHandler";
 import MapSector from "./mapSector";
 
-const baseWidth = 2048;
-const baseHeight = 1440;
-const width = baseWidth * 8;
-const height = baseHeight * 8;
+const totalWidth = 16384;
+const totalHeight = 11520;
+
+const numberOfChunks = 16;
+const totalChunks = numberOfChunks * numberOfChunks;
+const baseWidth = totalWidth / numberOfChunks;
+const baseHeight = totalHeight / numberOfChunks;
+const width = baseWidth * numberOfChunks;
+const height = baseHeight * numberOfChunks;
 
 const posStyling: React.CSSProperties = {
     position: "absolute",
@@ -31,14 +36,48 @@ const mapStyles: React.CSSProperties = {
 };
 
 export const posToSector = (x: number, y: number) =>
-    (Math.floor(y / baseHeight) * 8) + Math.floor(x / baseWidth);
+    (Math.floor(y / baseHeight) * numberOfChunks) + Math.floor(x / baseWidth);
 
 
 const bfsSectors: any[][] = [];
 const supsSectors: any[][] = [];
 
+function lineLine(x1: number, y1: number, x2: number, y2: number, x3: number, y3: number, x4: number, y4: number) {
+    // calculate the distance to intersection point
+    const uA = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+    const uB = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+
+    // if uA and uB are between 0-1, lines are colliding
+    return (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1);
+}
+
+/**
+ * check if line and rect intersect
+ * @param x1 line x1
+ * @param y1 line y1
+ * @param x2 line x2
+ * @param y2 line y2
+ * @param rx rect x
+ * @param ry rect y
+ * @param rw rect x size
+ * @param rh rect y size
+ * @returns 
+ */
+function lineRect(x1: number, y1: number, x2: number, y2: number, rx: number, ry: number, rw: number, rh: number) {
+    // check if the line has hit any of the rectangle's sides
+    // uses the Line/Line function below
+    const left = lineLine(x1, y1, x2, y2, rx, ry, rx, ry + rh);
+    const right = lineLine(x1, y1, x2, y2, rx + rw, ry, rx + rw, ry + rh);
+    const top = lineLine(x1, y1, x2, y2, rx, ry, rx + rw, ry);
+    const bottom = lineLine(x1, y1, x2, y2, rx, ry + rh, rx + rw, ry + rh);
+
+    // if ANY of the above are true, the line
+    // has hit the rectangle
+    return (left || right || top || bottom);
+}
+
 function addToSector(sectors: any[][], index: number, element: any) {
-    if (index > 64) return;
+    if (index > totalChunks) return;
     if (!sectors[index]) sectors[index] = [];
     sectors[index].push(element);
 }
@@ -47,23 +86,26 @@ battlefield.forEach((element: any) => {
     const index = posToSector(element.posx, element.posy);
     addToSector(bfsSectors, index, element);
     const edgeY = element.posy % baseHeight;
-    if (edgeY < 50) addToSector(bfsSectors, index - 8, element);
-    else if (edgeY > (baseHeight - 50)) addToSector(bfsSectors, index + 8, element);
+    if (edgeY < 20) addToSector(bfsSectors, index - numberOfChunks, element);
+    else if (edgeY > (baseHeight - 20)) addToSector(bfsSectors, index + numberOfChunks, element);
     const edgeX = element.posx % baseWidth;
-    if (edgeX < 50) addToSector(bfsSectors, index - 1, element);
-    else if (edgeX > (baseWidth - 50)) addToSector(bfsSectors, index + 1, element);
+    if (edgeX < 20) addToSector(bfsSectors, index - 1, element);
+    else if (edgeX > (baseWidth - 20)) addToSector(bfsSectors, index + 1, element);
 });
 
 supplyline.forEach((element: any) => {
     const index = posToSector(element.posx1, element.posy1);
     addToSector(supsSectors, index, element);
     const index2 = posToSector(element.posx2, element.posy2);
-    if (index !== index2) {
-        addToSector(supsSectors, index2, element);
-        const index3 = posToSector(element.posx1, element.posy2);
-        if (index3 !== index && index3 !== index2) addToSector(supsSectors, index3, element);
-        const index4 = posToSector(element.posx2, element.posy1);
-        if (index4 !== index && index4 !== index2 && index4 !== index3) addToSector(supsSectors, index4, element);
+    if (index !== index2) addToSector(supsSectors, index2, element);
+    for (let x = 0; x < numberOfChunks; x++) {
+        for (let y = 0; y < numberOfChunks; y++) {
+            const index = (y * numberOfChunks) + x;
+            if (lineRect(
+                element.posx1, element.posy1, element.posx2, element.posy2,
+                baseWidth * x, baseHeight * y, baseWidth, baseHeight
+            )) addToSector(supsSectors, index, element);
+        }
     }
 });
 
@@ -73,9 +115,9 @@ const Warmap = ({
     warmapEventHandler: WarmapEventHandler;
 }): JSX.Element => {
     const sectors = [];
-    for (let x = 0; x < 8; x++) {
-        for (let y = 0; y < 8; y++) {
-            const index = (y * 8) + x;
+    for (let x = 0; x < numberOfChunks; x++) {
+        for (let y = 0; y < numberOfChunks; y++) {
+            const index = (y * numberOfChunks) + x;
             if (bfsSectors[index] || supsSectors[index]) {
                 sectors.push(<MapSector
                     bfsSectors={bfsSectors}
