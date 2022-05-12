215caba7729779a305c7b702d90b636ee9f67be5
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-05-12 10:45:47 +0200

    Fixed codestyle

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,6 @@ import Warmap from './map/warmap';
 const warmapEventHandler = new WarmapEventHandler();
 const rootElement = document.getElementById('root');
 if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(<Warmap warmapEventHandler={warmapEventHandler} />);
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(<Warmap warmapEventHandler={warmapEventHandler} />);
 } else console.error("root element not found!");
