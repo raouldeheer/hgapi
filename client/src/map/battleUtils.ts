4b544f5f8bd32294af057a19f4c6ef2c7b3a8313
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-05-15 15:51:35 +0200

    Refactored battleUtils

--- client/src/map/battleUtils.ts
+++ client/src/map/battleUtils.ts
@@ -3,16 +3,7 @@ import { MissionStatus } from "./mapInterfaces";
 
 const enum ResourceTypes {
     infantryman = 1,
-    transport_plane = 10,
-    motorcycle = 11,
-    light_armor = 12,
-    terrain_vehicle = 15,
-    armoured_car = 16,
-    heavy_armor = 17,
-    heavy_tank_destroyer = 18,
     fighter_pilot = 2,
-    fighter_plane = 20,
-    recon_plane = 21,
     heavy_fighter_plane = 3,
     paratrooper = 4,
     tank_crew = 5,
@@ -20,6 +11,15 @@ const enum ResourceTypes {
     medium_armor = 7,
     tank_destroyer = 8,
     armored_personnel_carrier = 9,
+    transport_plane = 10,
+    motorcycle = 11,
+    light_armor = 12,
+    terrain_vehicle = 15,
+    armoured_car = 16,
+    heavy_armor = 17,
+    heavy_tank_destroyer = 18,
+    fighter_plane = 20,
+    recon_plane = 21,
 }
 
 export const enum BattleType {
@@ -40,10 +40,9 @@ export function battleIdToColor(
     warmapEventHandler: WarState,
     battleId: string | undefined,
     battleType: BattleType,
-    defaultColor?: string
+    battleColor: string = BattleColor.Default
 ) {
     const battle = warmapEventHandler.GetBattle(battleId);
-    let battleColor = defaultColor || BattleColor.Default;
     if (battle && battle.MissionDetails.response === 0) {
         const status = battle.MissionDetails.info?.status;
         switch (status) {
@@ -59,8 +58,8 @@ export function battleIdToColor(
                 }, new Map());
                 if (!totalResources) return battleColor;
                 if (!battleIsFun(totalResources, battleType)) battleColor = BattleColor.NotFun;
-                if (battle.MissionDetails.factions.reduce(
-                    (prev, curr) => prev + (currFaction !== curr.factionId ? curr?.players?.length : 0 || 0), 0
+                if (battle.MissionDetails.factions.reduce((prev, curr) =>
+                    prev + (currFaction !== curr.factionId ? curr?.players?.length : 0 || 0), 0
                 ) >= 4) battleColor = BattleColor.Queued;
                 break;
             case MissionStatus.MissionRunning:
@@ -77,7 +76,10 @@ export function battleIdToColor(
 function battleIsFun(totalResources: Map<number, number>, battleType: BattleType) {
     const ticketsRequired = battleType === BattleType.Assault ? 18 : 12;
 
-    const getValue = (type: ResourceTypes, divider: number) => Math.floor((totalResources.get(type) || 0) / divider);
+    const getValue = (type: ResourceTypes, divider: number) =>
+        Math.floor((totalResources.get(type) || 0) / divider);
+    const getValues = (types: ResourceTypes[], divider: number) =>
+        types.reduce((prev, curr) => prev + getValue(curr, divider), 0);
 
     const ticketsInfantry = getValue(ResourceTypes.infantryman, 12);
 
@@ -86,26 +88,29 @@ function battleIsFun(totalResources: Map<number, number>, battleType: BattleType
         getValue(ResourceTypes.paratrooper, 12),
         getValue(ResourceTypes.transport_plane, 4));
 
+    const InfParaTickets = ticketsInfantry + ticketsParas + ticketsParasAirborne;
+
     const ticketsTotal = battleType === BattleType.Skirmish
         ? ticketsInfantry
-        : (ticketsInfantry
-            + ticketsParas
-            + ticketsParasAirborne
+        : (InfParaTickets
             + getValue(ResourceTypes.recon, 10)
             + Math.min(
                 getValue(ResourceTypes.tank_crew, 10),
-                getValue(ResourceTypes.light_armor, 10) // ticketsTanks
-                + getValue(ResourceTypes.medium_armor, 10)
-                + getValue(ResourceTypes.heavy_armor, 10)
-                + getValue(ResourceTypes.tank_destroyer, 10)
-                + getValue(ResourceTypes.heavy_tank_destroyer, 10)
+                getValues([
+                    ResourceTypes.medium_armor,
+                    ResourceTypes.heavy_armor,
+                    ResourceTypes.tank_destroyer,
+                    ResourceTypes.heavy_tank_destroyer,
+                    ResourceTypes.light_armor
+                ], 10) // ticketsTanks
             ) + Math.min(
                 getValue(ResourceTypes.fighter_pilot, 10),
-                getValue(ResourceTypes.recon_plane, 10) // ticketsPlanes
-                + getValue(ResourceTypes.fighter_plane, 10)
-                + getValue(ResourceTypes.heavy_fighter_plane, 10)
+                getValues([
+                    ResourceTypes.fighter_plane,
+                    ResourceTypes.heavy_fighter_plane,
+                    ResourceTypes.recon_plane
+                ], 10) // ticketsPlanes
             ));
 
-    return ((ticketsInfantry + ticketsParas + ticketsParasAirborne) >= (ticketsRequired / 2))
-        && (ticketsTotal >= ticketsRequired);
+    return InfParaTickets >= (ticketsRequired / 2) && ticketsTotal >= ticketsRequired;
 }
