5c2c9ad837e4a5722f74920b0d1e9a10c7a0a2a6
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-05-15 16:44:29 +0200

    Cleaned up warmap

--- client/src/map/warmap.tsx
+++ client/src/map/warmap.tsx
@@ -2,8 +2,10 @@ import image from "hagcp-assets/images/background.png";
 // @ts-expect-error
 import { MapInteractionCSS } from "react-map-interaction";
 import React, { useState } from "react";
+import { Stage, Layer } from "react-konva";
+import BattlefieldPoint from "./battlefieldPoint";
+import Supplyline from "./supplyline";
 import { WarState } from "../warmapEventHandler";
-import MapSector from "./mapSector";
 import sectorsToDraw from "../json/sectors.json";
 import Settings from "../settings";
 
@@ -13,8 +15,6 @@ const totalHeight = 11520;
 const numberOfChunks = 16;
 const baseWidth = totalWidth / numberOfChunks;
 const baseHeight = totalHeight / numberOfChunks;
-const width = baseWidth * numberOfChunks;
-const height = baseHeight * numberOfChunks;
 
 const posStyling: React.CSSProperties = {
     position: "absolute",
@@ -26,48 +26,49 @@ const componentStyling: React.CSSProperties = {
     ...posStyling,
     width: "100%",
     height: "100%",
-    zIndex: "-1",
 };
 
 const mapStyles: React.CSSProperties = {
     ...posStyling,
-    width: `${width}px`,
-    height: `${height}px`
+    width: `${baseWidth * numberOfChunks}px`,
+    height: `${baseHeight * numberOfChunks}px`
 };
 
-const Warmap = ({
-    warState
-}: {
-    warState: WarState;
-}): JSX.Element => {
+const Warmap = ({ warState }: { warState: WarState; }): JSX.Element => {
     const [warid, setWarid] = useState("");
     warState.onNewWar = setWarid;
 
     const sectors = [];
     for (let x = 0; x < numberOfChunks; x++) {
         for (let y = 0; y < numberOfChunks; y++) {
-            const index = (y * numberOfChunks) + x;
-            const sectorInList = sectorsToDraw.findIndex(v => v.index === index);
+            const sectorInList = sectorsToDraw.findIndex(v => v.index === ((y * numberOfChunks) + x));
             if (sectorInList !== -1) {
-                sectors.push(<MapSector
-                    posx={baseWidth}
-                    posy={baseHeight}
-                    offsetx={baseWidth * x}
-                    offsety={baseHeight * y}
-                    sectorData={sectorsToDraw[sectorInList]}
-                    warState={warState}
-                    key={index}
-                />);
+                const sectorData = sectorsToDraw[sectorInList];
+                sectors.push(<Stage
+                    style={{
+                        position: "absolute",
+                        top: `${baseHeight * y}px`,
+                        left: `${baseWidth * x}px`,
+                        width: `${baseWidth}px`,
+                        height: `${baseHeight}px`
+                    }}
+                    key={`sector${sectorData.index}`}
+                    width={baseWidth}
+                    height={baseHeight}
+                    offsetX={baseWidth * x}
+                    offsetY={baseHeight * y}
+                    listening={false}>
+                    <Layer listening={false}>
+                        {sectorData.supsSector.map(e => <Supplyline key={e} id={e} warState={warState} />)}
+                        {sectorData.bfsSector.map(e => <BattlefieldPoint key={e} id={e} warState={warState} />)}
+                    </Layer>
+                </Stage>);
             }
         }
     }
 
     return <div style={componentStyling}>
-        <MapInteractionCSS minScale={0.10}
-            defaultValue={{
-                scale: 0.10,
-                translation: { x: 0, y: 0, },
-            }}>
+        <MapInteractionCSS minScale={0.10} defaultValue={{ scale: 0.10, translation: { x: 0, y: 0, }, }}>
             <img id={warid} src={image} style={mapStyles} alt="background map" />
             {sectors}
         </MapInteractionCSS>
