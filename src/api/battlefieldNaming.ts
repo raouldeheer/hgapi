82b7bab80d5364f37c20b8ee16133778b72f7d2d
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-05-17 14:09:49 +0200

    Cleaned up battlefieldNaming.ts

--- src/api/battlefieldNaming.ts
+++ src/api/battlefieldNaming.ts
@@ -2,30 +2,27 @@ import { KeyValueChangeKey } from "hagcp-network-client";
 import { DataStore } from "hagcp-utils";
 import { MapPoint, Battlefield, Accesspoint, AccesspointTemplate, Supplyline } from "../interfaces";
 
+const notFound = () => { throw 404; };
+const getItemStoreArray = <T>(datastore: DataStore, name: KeyValueChangeKey) =>
+    Array.from<T>(datastore.GetItemStore<T>(name)?.values() || []);
+
 export const getResolveTitle = (datastore: DataStore) => (bftitle: string): MapPoint => {
     if (bftitle.includes(" - ")) {
-        const battlefield = Array.from(datastore.GetItemStore<Battlefield>(KeyValueChangeKey.battlefield)?.values() || [])
-            .find(value => value.bftitle === bftitle.split(" - ")[0]);
-        if (!battlefield) throw 404;
+        const battlefield = getItemStoreArray<Battlefield>(datastore, KeyValueChangeKey.battlefield)
+            .find(value => value.bftitle === bftitle.split(" - ")[0]) || notFound();
 
-        const accesspoint = Array.from(datastore.GetItemStore<Accesspoint>(KeyValueChangeKey.accesspoint)?.values() || [])
-            .find(value => {
-                if (value.battlefield === battlefield.id) {
-                    const template = datastore.GetData<AccesspointTemplate>(KeyValueChangeKey.accesspointtemplate, value.template);
-                    return bftitle === `${battlefield.bftitle} - ${template.abbr}`;
-                }
-                return false;
-            });
-        if (!accesspoint) throw 404;
+        const accesspoint = getItemStoreArray<Accesspoint>(datastore, KeyValueChangeKey.accesspoint)
+            .find(value => value.battlefield === battlefield.id
+                ? bftitle === `${battlefield.bftitle} - ${datastore.GetData<AccesspointTemplate>(KeyValueChangeKey.accesspointtemplate, value.template).abbr}`
+                : false
+            ) || notFound();
 
-        const supplyline = Array.from(datastore.GetItemStore<Supplyline>(KeyValueChangeKey.supplyline)?.values() || [])
-            .find(value => (value.accesspoint1Id === accesspoint.id) || (value.accesspoint2Id === accesspoint.id));
-        if (!supplyline) throw 404;
+        const supplyline = getItemStoreArray<Supplyline>(datastore, KeyValueChangeKey.supplyline)
+            .find(value => (value.accesspoint1Id === accesspoint.id) || (value.accesspoint2Id === accesspoint.id)) || notFound();
         return supplyline;
     } else {
-        const battlefield = Array.from(datastore.GetItemStore<Battlefield>(KeyValueChangeKey.battlefield)?.values() || [])
-            .find(value => value.bftitle === bftitle);
-        if (!battlefield) throw 404;
+        const battlefield = getItemStoreArray<Battlefield>(datastore, KeyValueChangeKey.battlefield)
+            .find(value => value.bftitle === bftitle) || notFound();
         return battlefield;
     }
 };
@@ -33,19 +30,12 @@ export const getResolveTitle = (datastore: DataStore) => (bftitle: string): MapP
 export const getToBFTitle = (datastore: DataStore) => (id: string): string => {
     const result = datastore.GetData<Battlefield>(KeyValueChangeKey.battlefield, id)?.bftitle;
     if (result) return result;
-    const supplyline = datastore.GetData<Supplyline>(KeyValueChangeKey.supplyline, id);
-    if (!supplyline) throw 404;
-    const ap1 = datastore.GetData<Accesspoint>(KeyValueChangeKey.accesspoint, supplyline.accesspoint1Id);
-    if (!ap1) throw 404;
-    const ap2 = datastore.GetData<Accesspoint>(KeyValueChangeKey.accesspoint, supplyline.accesspoint2Id);
-    if (!ap2) throw 404;
-    const apt1 = datastore.GetData<AccesspointTemplate>(KeyValueChangeKey.accesspointtemplate, ap1.template);
-    if (!apt1) throw 404;
-    const apt2 = datastore.GetData<AccesspointTemplate>(KeyValueChangeKey.accesspointtemplate, ap2.template);
-    if (!apt2) throw 404;
-    const apb1 = datastore.GetData<Battlefield>(KeyValueChangeKey.battlefield, ap1.battlefield);
-    if (!apb1) throw 404;
-    const apb2 = datastore.GetData<Battlefield>(KeyValueChangeKey.battlefield, ap2.battlefield);
-    if (!apb2) throw 404;
+    const supplyline = datastore.GetData<Supplyline>(KeyValueChangeKey.supplyline, id) || notFound();
+    const ap1 = datastore.GetData<Accesspoint>(KeyValueChangeKey.accesspoint, supplyline.accesspoint1Id) || notFound();
+    const ap2 = datastore.GetData<Accesspoint>(KeyValueChangeKey.accesspoint, supplyline.accesspoint2Id) || notFound();
+    const apt1 = datastore.GetData<AccesspointTemplate>(KeyValueChangeKey.accesspointtemplate, ap1.template) || notFound();
+    const apt2 = datastore.GetData<AccesspointTemplate>(KeyValueChangeKey.accesspointtemplate, ap2.template) || notFound();
+    const apb1 = datastore.GetData<Battlefield>(KeyValueChangeKey.battlefield, ap1.battlefield) || notFound();
+    const apb2 = datastore.GetData<Battlefield>(KeyValueChangeKey.battlefield, ap2.battlefield) || notFound();
     return `${apb1.bftitle} - ${apt1.abbr} or ${apb2.bftitle} - ${apt2.abbr}`;
 };
