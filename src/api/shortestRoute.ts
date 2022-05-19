567fc5423d268293be91621160e504dedd16766a
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-05-19 14:51:12 +0200

    Fixed planeGraph

--- src/api/shortestRoute.ts
+++ src/api/shortestRoute.ts
@@ -56,59 +56,65 @@ export function shortestRoute(app: Express, config: APIConfig) {
     });
 
     const battlefields = datastore.GetItemStore<Battlefield>("battlefield") || [];
-    const airfields = new Set(Array.from(battlefields.values())
+    const airfields = new Map(Array.from(battlefields.values())
         .filter(element => element.gamemap === "204" || element.gamemap === "205")
-        .map(e => e.id));
+        .map(e => ([e.id, e])));
+    const distances = new Map([3450, 3600, 3800, 2850, 1850].map(distance => {
+        const planeGraph = new DijkstraCalculator;
+
+        // Add airfields
+        battlefields.forEach(element => {
+            if (element.gamemap === "204" || element.gamemap === "205")
+                planeGraph.addVertex(element.id);
+        });
+
+        // Add routes
+        for (const [key1, bf1] of airfields) {
+            for (const [key2, bf2] of airfields) {
+                if (key1 === key2) continue;
+                const distanceBetween = Math.hypot(bf2.posx - bf1.posx, bf2.posy - bf1.posy);
+                if (distanceBetween < distance) planeGraph.addEdge(key1, key2, distanceBetween);
+            }
+        }
+
+        return [distance, planeGraph];
+    }));
 
     app.get("/planeroute", (req: Request, res: Response) => {
         res.set("Cache-control", "public, max-age=300");
         if (req.query.distance) {
             const distance = Number(req.query.distance);
+            const planeGraph = distances.get(distance);
+            if (planeGraph) {
+                const getPlaneResult = (from: string, to: string) => ({
+                    path: planeGraph.calculateShortestPath(from, to),
+                    distance: planeGraph.calculateShortestPathAsLinkedListResult(from, to)
+                        .map(value => planeGraph.adjacencyList[value.source]
+                            .find(item => item.id === value.target)?.weight || 0)
+                        .reduce((prev, curr) => prev + curr, 0),
+                });
 
-            const planeGraph = new DijkstraCalculator;
-
-            // Add airfields
-            battlefields.forEach(element => {
-                if (element.gamemap === "204" || element.gamemap === "205")
-                    planeGraph.addVertex(element.id);
-            });
-
-            for (const [key1, bf1] of battlefields) {
-                for (const [key2, bf2] of battlefields) {
-                    if (key1 === key2) continue;
-                    const distanceBetween = Math.hypot(bf2.posx - bf1.posx, bf2.posy - bf1.posy);
-                    if (distanceBetween < distance) mapGraph.addEdge(key1, key2, distanceBetween);
-                }
-            }
-
-            const getPlaneResult = (from: string, to: string) => ({
-                path: mapGraph.calculateShortestPath(from, to),
-                distance: mapGraph.calculateShortestPathAsLinkedListResult(from, to)
-                    .map(value => mapGraph.adjacencyList[value.source]
-                        .find(item => item.id === value.target)?.weight || 0)
-                    .reduce((prev, curr) => prev + curr, 0),
-            });
-
-            if (req.query.id1 && req.query.id2) {
-                const id1 = String(req.query.id1);
-                const id2 = String(req.query.id2);
-                if (/^\d+$/.test(id1) && /^\d+$/.test(id2) && airfields.has(id1) && airfields.has(id2)) {
-                    res.json(getPlaneResult(id1, id2));
+                if (req.query.id1 && req.query.id2) {
+                    const id1 = String(req.query.id1);
+                    const id2 = String(req.query.id2);
+                    if (/^\d+$/.test(id1) && /^\d+$/.test(id2) && airfields.has(id1) && airfields.has(id2)) {
+                        res.json(getPlaneResult(id1, id2));
+                        return;
+                    }
+                } else if (req.query.bftitle1, req.query.bftitle2) {
+                    try {
+                        const id1 = resolveTitle(String(req.query.bftitle1)).id;
+                        const id2 = resolveTitle(String(req.query.bftitle2)).id;
+                        if (airfields.has(id1) && airfields.has(id2)) {
+                            res.json(getPlaneResult(id1, id2));
+                        } else {
+                            res.sendStatus(412);
+                        }
+                    } catch (error) {
+                        if (typeof error == "number") res.sendStatus(error);
+                    }
                     return;
                 }
-            } else if (req.query.bftitle1, req.query.bftitle2) {
-                try {
-                    const id1 = resolveTitle(String(req.query.bftitle1)).id;
-                    const id2 = resolveTitle(String(req.query.bftitle2)).id;
-                    if (airfields.has(id1) && airfields.has(id2)) {
-                        res.json(getPlaneResult(id1, id2));
-                    } else {
-                        res.sendStatus(412);
-                    }
-                } catch (error) {
-                    if (typeof error == "number") res.sendStatus(error);
-                }
-                return;
             }
         }
         res.sendStatus(412);
