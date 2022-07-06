ac10a82740ac1ebf19eab456741ed99f6fcd8d58
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-07-06 21:07:32 +0200

    Added queuestats endpoint

--- src/api/serverLoad.ts
+++ src/api/serverLoad.ts
@@ -18,4 +18,48 @@ export function serverLoad(app: Express, config: APIConfig) {
         }
         res.sendStatus(412);
     });
+
+    app.get("/queuestats", async (_, res) => {
+        if (!client) {
+            res.sendStatus(503);
+            return;
+        }
+        const data = await client.sendPacketAsync<{ requestType: 0; }, any>(ClassKeys.MonitorLoadRequest, { requestType: 0, });
+        if (data) {
+            const factions = new Map<string, Map<String, any[]>>([
+                ["1", new Map],
+                ["2", new Map],
+                ["3", new Map],
+            ]);
+
+            const getSubData = (factionId: string, id: number) =>
+                ((factions.get(factionId) || new Map)
+                    .get(id.toString()) || [])
+                    .reduce((prev: number, curr: { count: number; }) => prev + curr.count, 0);
+
+            data?.waitingTimes.filter((item: { playerTier: number; }) => item.playerTier >= 2)
+                .filter((item: { onlyWarMissions: any; }) => item.onlyWarMissions)
+                .forEach((item: { factionTemplateId: string; characterTemplateId: String; }) => {
+                    const characterMap = factions.get(item.factionTemplateId) || new Map;
+                    const queueList = characterMap.get(item.characterTemplateId) || [];
+                    queueList.push(item);
+                    characterMap.set(item.characterTemplateId, queueList);
+                    factions.set(item.factionTemplateId, characterMap);
+                });
+
+            const result: any = {};
+            ["1", "2", "3"].forEach(factionId => {
+                const factionObj = {};
+                Reflect.set(factionObj, "rifleman", getSubData(factionId, 1));
+                Reflect.set(factionObj, "fighterpilot", getSubData(factionId, 2));
+                Reflect.set(factionObj, "paratrooper", getSubData(factionId, 4));
+                Reflect.set(factionObj, "tankcrew", getSubData(factionId, 5));
+                Reflect.set(factionObj, "recon", getSubData(factionId, 6));
+                Reflect.set(result, factionId, factionObj);
+            });
+            res.json(result);
+            return;
+        }
+        res.sendStatus(412);
+    });
 }
